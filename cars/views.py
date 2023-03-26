2200951530c8951fea3f3b13a6de1d958679f119
unknown <sean.herbison@gmail.com>
2023-03-26 14:51:28 -0700

    Completed POST Functionality for Car and Updated Rest Framework Decimal Settings to return Float instead of String.

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CarSerializer
 from .models import Car
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
-    cars = Car.objects.all()
-    serializer = CarSerializer(cars, many=True)
+
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
     
-    return Response(serializer.data)
+    if request.method == 'POST':
+         serializer = CarSerializer(data=request.data)
+         serializer.is_valid(raise_exception=True)
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
+         
+
+    #Previous way
+    # if request.method == 'POST':
+    #     serializer = CarSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     else:
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
